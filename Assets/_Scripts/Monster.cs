18ed1e0ed46c2a700ff3a5ea01f7104f0ba2902b
Jhlee <ljh990627@gmail.com>
2026-02-04 11:05:46 +0900

    씬 이름 변경 (SampleScene -> NavMeshTest)
    Navmesh를 제대로 동작 시킬 방법 연구 필요

---
+++ Assets/_Scripts/Monster.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] Transform _target;     // 타겟
+    NavMeshAgent _agent;                    // 네비메시 에이전트
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.updateRotation = false;  // 회전 비활성화
+        _agent.updateUpAxis = false;    // Y축 회전 비활성화
+    }
+
+    void Update()
+    {
+        // 타겟 위치로 이동
+        _agent.SetDestination(_target.position);
+    }
+}
