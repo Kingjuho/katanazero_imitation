330c3ce95cbecd3758c644278d904d76a67a55ee
Jhlee <ljh990627@gmail.com>
2026-02-05 11:41:00 +0900

    계단과 접촉 시 Player의 중력 0으로 변경
    아래로 점점 미끄러지는 문제 해결 필요

---
+++ Assets/_Scripts/Backgrounds/Stair.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Stair : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            player.GetComponent<Rigidbody2D>().gravityScale = 0;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            player.GetComponent<Rigidbody2D>().gravityScale = 1;
+    }
+}
