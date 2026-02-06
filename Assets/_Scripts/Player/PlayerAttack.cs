e4db71a90ae25a154edc5e0c806f8e10b5591cf6
Jhlee <ljh990627@gmail.com>
2026-02-06 09:22:11 +0900

    플레이어 공격 모션 구현
    Animator Trigger 사용

---
+++ Assets/_Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    Animator _anim;
+
+    void Start()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        Attack();
+    }
+
+    void Attack()
+    {
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        _anim.SetTrigger("Attack");
+    }
+}
--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(PlayerMovement))]
+[RequireComponent(typeof(PlayerAttack))]
 
 public class PlayerController : MonoBehaviour
 {
     // PlayerHealth _health;           // 체력 컴포넌트
     PlayerMovement _movement;       // 이동 컴포넌트
+    PlayerAttack _attack;           // 공격 컴포넌트
     
 
     void Start()
     {
         _movement = GetComponent<PlayerMovement>();
+        _attack = GetComponent<PlayerAttack>();
     }
 
     void Update()
